f63184f83a03a4ab48ae2bb7e2641d95b16e26c4
drenassi <drenassi@student.42perpignan.fr>
2024-04-04 08:40:54 +0200

    ex00 done

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: drenassi <@student.42perpignan.fr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/03 19:13:11 by drenassi          #+#    #+#             */
+/*   Updated: 2024/04/03 19:41:08 by drenassi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int main(int ac, char **av)
+{
+    if (ac == 1)
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+    for (int i = 1 ; av[i] != NULL; i++)
+    {
+        for (int j = 0 ; av[i][j] != '\0' ; j++)
+            std::cout << (char) std::toupper(av[i][j]);
+        std::cout << ' ';
+    }
+    std::cout << std::endl;
+    return (0);
+}
