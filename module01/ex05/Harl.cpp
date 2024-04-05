ff4f1ba2987251e705405b124f278a9280948eac
drenassi <dada006007@gmail.com>
2024-04-05 21:33:18 +0200

    ex05 done

--- module01/ex05/Harl.cpp
+++ module01/ex05/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: drenassi <@student.42perpignan.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/05 20:12:05 by drenassi          #+#    #+#             */
-/*   Updated: 2024/04/05 21:03:24 by drenassi         ###   ########.fr       */
+/*   Updated: 2024/04/05 21:24:34 by drenassi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,27 +26,27 @@ void	Harl::debug()
 {
 	std::cout << "\tHarl says:\n\t" << GREEN << "\"I love having extra ";
 	std::cout << "bacon for my 7XL-double-cheese-triple-pickle-specialketchup ";
-	std::cout << "burger. I really do!\"" << END_COLOR << std::endl;
+	std::cout << "burger.\nI really do!\"" << END_COLOR << std::endl;
 }
 
 void	Harl::info()
 {
 	std::cout << "\tHarl says:\n\t" << YELLOW << "\"I cannot believe adding extra ";
-	std::cout << "bacon costs more money. You didn't put enough bacon in my ";
-	std::cout << "burger! If you did, I wouldn't be asking for more!\"";
+	std::cout << "bacon costs more money.\nYou didn't put enough bacon in my ";
+	std::cout << "burger!\nIf you did, I wouldn't be asking for more!\"";
 	std::cout << END_COLOR << std::endl;
 }
 
 void	Harl::warning()
 {
 	std::cout << "\tHarl says:\n\t" << RED << "\"I think I deserve to have some ";
-	std::cout << "extra bacon for free. I've been coming for years whereas you ";
+	std::cout << "extra bacon for free.\nI've been coming for years whereas you ";
 	std::cout << "started working here since last month.\"" << END_COLOR << std::endl;
 }
 
 void	Harl::error()
 {
-	std::cout << "\tHarl says:\n\t" << MAGENTA << "\"This is unacceptable! I want ";
+	std::cout << "\tHarl says:\n\t" << MAGENTA << "\"This is unacceptable!\nI want ";
 	std::cout << "to speak to the manager now.\"" << END_COLOR << std::endl;
 }
 
